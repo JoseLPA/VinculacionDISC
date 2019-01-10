d6644b89e75c1bf352e0a0d1b95aa16bcfa25114
MaikelCarvajal <46094823+MaikelCarvajal@users.noreply.github.com>
2019-01-10 04:21:57 -0300

    Actualizacion de las rutas
    Se actualizan las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,15 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('AdmConvenio','ConvenioController@index')->name('AdmConvenio');
 Route::resource('convenio','ConvenioController');
-//web
+Route::get('AdmActividadExtension','ActividadExtensionController@index')->name('AdmActividadExtension');
+Route::resource('actividadExtension','ActividadExtensionController');
+Route::get('AdmAprendizajeServicio','AprendizajeServicioController@index')->name('AdmAprendizajeServicio');
+Route::resource('aprendizajeServicio','AprendizajeServicioController');
+Route::get('AdmTitulado','TituladoController@index')->name('AdmTitulado');
+Route::resource('titulado','TituladoController');
+Route::get('AdmActividadTitulacion','ActividadTitulacionController@index')->name('AdmActividadTitulacion');
+Route::resource('actividadTitulacion','ActividadTitulacionController');
